8e2d24a808e46ade5d63365adbe46e78a4fe8666
krishnayadav1cs26 <krishna.yadav1_cs26@gla.ac.in>
2026-09-15 21:38:37 +0530

    Add files via upload

---
+++ C/08.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int i = 20;
+    int k = i;
+    int a = 26, b = 5, c = 3, d = 17;
+
+    printf("The value of i is %d and value of j is %d\n",i, k);    
+    printf("The value of a is %d and value of b is %d\n",a, b);    
+    printf("The value of c is %d and value of d is %d\n",c, d);   
+    // modulus operator is used to get the remainder
+    printf("The remaider when a is divided by b is %d\n",a%b);    
+    
+
+    return 0;
+}
