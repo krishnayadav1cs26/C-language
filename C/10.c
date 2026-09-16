a10133b6795d875b422d6f9e7be98ae5658e23ed
krishnayadav1cs26 <krishna.yadav1_cs26@gla.ac.in>
2026-09-16 20:10:45 +0530

    Add files via upload

---
+++ C/10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    // explain step by step evalution of 3*x/y - z+k, where x = 5, y=8, z=7, k=9
+    int x = 5, y = 8, z = 7, k = 9;
+    int e = 3*x/y - z+k;
+    printf("The value of e is %d", e);
+    return 0;
+}
