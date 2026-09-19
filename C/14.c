3d930174bfa3f1a03bbb591b8ea1152a8bfe1c9a
krishnayadav1cs26 <krishna.yadav1_cs26@gla.ac.in>
2026-09-19 20:56:13 +0530

    Add files via upload

---
+++ C/14.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    // condition ? expression-if-true : expression-if-flase
+    int a = 400;
+    int b = 359;
+    a>b?printf("a is the greater"):printf("b is greater");
+    return 0;
+}
